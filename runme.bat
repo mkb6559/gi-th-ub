6725a1179631a3f9b62405e2cb92261bb611acac
mkb6559 <90779603+mkb6559@users.noreply.github.com>
2023-04-04 14:54:05 -0400

    like gathpacho

--- runme.bat
+++ runme.bat
@@ -1,4 +1,2 @@
 @printf "            ----------\n           /          \ \n          /            \ \n         /              \ \n      gi|----------------|ub\n         \              /\n          \            /\n           \          /\n            ----------\n (a new angular implimentation of github [in radians of course])"
-
-
 pause
