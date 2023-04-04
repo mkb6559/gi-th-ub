089c1eb3fe4e1c5b363738d17973721d759db5f6
mkb6559 <90779603+mkb6559@users.noreply.github.com>
2023-04-04 14:53:12 -0400

    Create runme.bat
    like gathpacho

---
+++ runme.bat
@@ -0,0 +1,4 @@
+@printf "            ----------\n           /          \ \n          /            \ \n         /              \ \n      gi|----------------|ub\n         \              /\n          \            /\n           \          /\n            ----------\n (a new angular implimentation of github [in radians of course])"
+
+
+pause
